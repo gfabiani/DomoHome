1bc24ba9e5ce44058b97ec51e7d166b001abf0aa
Giovanni Fabiani <fabiani.giovanni@gmail.com>
2015-11-15 11:49:16 +0100

    project updated

---
+++ app/src/main/java/com/fabiani/domohome/app/Controller/MultipleFragmentsActivity.java
@@ -0,0 +1,16 @@
+package com.fabiani.domohome.app.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.fabiani.domohome.app.R;
+
+public class MultipleFragmentsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_fragments);
+    }
+}
