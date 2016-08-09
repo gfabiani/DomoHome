fbc8c13965d3aa6a52280bf8af0d62d41db6215b
Giovanni Fabiani <fabiani.giovanni@gmail.com>
2016-08-09 19:05:58 +0200

    Updating video

--- app/src/main/java/com/fabiani/domohome/app/Controller/VideoFragment.java
+++ app/src/main/java/com/fabiani/domohome/app/Controller/VideoFragment.java
@@ -2,8 +2,6 @@ package com.fabiani.domohome.app.controller;
 
 import android.app.Fragment;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,24 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import com.fabiani.domohome.app.R;
 import com.fabiani.domohome.app.model.Dashboard;
 import com.fabiani.domohome.app.model.VideoFetchr;
-
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class VideoFragment extends Fragment {
+    private final int NUMBER_OF_JOBS = 10;
     private ImageView mImageView;
     private Toolbar mToolbar;
-    ;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new VideoFetchTask().execute();
+        VideoFetchr.sendVideoOpen();
+        executeJobs(NUMBER_OF_JOBS);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,30 +40,30 @@ public class VideoFragment extends Fragment {
         return v;
     }
 
-    public class VideoFetchTask extends AsyncTask<Void, Void, Bitmap> {
-
-        @Override
-        protected Bitmap doInBackground(Void... params) {
-            byte[] b = new byte[0];
-            try {
-                b = new VideoFetchr().getUrlBytes("https://" + Dashboard.sIp + "/telecamera.php");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
-            return bitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap result) {
-            mImageView.setImageBitmap(result);
-
-        }
-    }
-
     public static VideoFragment newInstance() {
         VideoFragment fragment = new VideoFragment();
         return fragment;
+    }
+
+    class VideoThread implements Runnable {
+        Bitmap bitmap;
+
+        @Override
+        public void run() {
+            try {
+                bitmap = new VideoFetchr().getUrlBitmap("https://" + Dashboard.sIp + "/telecamera.php");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mImageView.post(() -> mImageView.setImageBitmap(bitmap));
+        }
+    }
+
+    private void executeJobs(int n) {
+        ExecutorService ex = Executors.newFixedThreadPool(1);
+        for (int i = 0; i < n; i++)
+            ex.execute(new VideoThread());
+        ex.shutdown();
     }
 }
 
